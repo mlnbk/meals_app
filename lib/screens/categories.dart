00d6b42f6ed96073757d14ef83340d753a7eb9ad
mlnbk <dev.milan.bako@gmail.com>
2023-07-30 17:38:39 +0200

    feat: show meals for category
    * navigate to meal list on category tap
    * add MealItem widget
    * use `transparent_image` for loading images

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MealsScreen(
+          meals: filteredMeals,
+          title: category.title,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +39,10 @@ class CategoriesScreen extends StatelessWidget {
           childAspectRatio: 3 / 2,
         ),
         children: availableCategories
-            .map((category) => CategoryGridItem(category: category))
+            .map((category) => CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () => _selectCategory(context, category),
+                ))
             .toList(),
       ),
     );
