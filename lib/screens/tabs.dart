c1af24f99c260aed956825d1cbefc4e9314365d1
mlnbk <dev.milan.bako@gmail.com>
2023-08-01 21:35:03 +0200

    chore: remove unused imports

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:meals_app/data/dummy_data.dart';
-import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/providers/favorites_provider.dart';
 import 'package:meals_app/providers/meals_provider.dart';
 import 'package:meals_app/screens/categories.dart';
